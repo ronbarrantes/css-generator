7d837e1200bf3ce6981eba9b53d9b3e873f97217
ronbarrantes <ronbarrantes@gmail.com>
2021-10-20 00:49:05 -0700

    FIX: issue with the output path

--- utils/path-parser.js
+++ utils/path-parser.js
@@ -5,11 +5,9 @@ const workingDir = process.cwd()
 
 const defaultCssFile = 'AUTO-GENERATED.css'
 
-
 export const paths = {
     input: '',
     output: '',
-    outputRel: ''
 }
 
 for(let i = 0; i < options.length; i += 2 ){
@@ -17,21 +15,15 @@ for(let i = 0; i < options.length; i += 2 ){
     const optionValue = options[i + 1]
     const extName = path.extname(optionValue).toLowerCase()
 
-    if(optionKey === '--input-file' && paths.input.length === 0){
+    if((optionKey === '--input-file' || optionKey === '-i' ) && paths.input.length === 0){
         if(extName !== '.html')
             throw new Error('Not a valid html file')
 
         paths.input = path.join(workingDir, optionValue)
     }
 
-    if(optionKey === '--output-file' && paths.output.length === 0){
-        if(extName !== '.css')
-            throw new Error('Not a valid css file')
-        paths.outputRel = optionValue
-        paths.output = path.join(workingDir, optionValue)
-    }
+    // TODO: Make a output file option (that's safe)
 }
 
-// TODO: fix the default so that the outputRel always shows up in the correct dir
 if(paths.output === '')
-    paths.output = path.join(workingDir, defaultCssFile) 
+    paths.output = path.join(path.dirname(paths.input), defaultCssFile) 
